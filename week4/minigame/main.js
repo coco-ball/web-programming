c633dbfff220dd2fd974cfbd3d11695dcc78efa3
coco-ball <kidbean02@snu.ac.kr>
2023-03-28 16:22:50 +0900

    기록 추가 & 가운데 정렬

--- week4/minigame/main.js
+++ week4/minigame/main.js
@@ -3,7 +3,12 @@ let count = 0;
 
 function checkAnswer() {
   const guess = document.getElementById("answer").value;
+  // console.log(guess);
+  // console.log(typeof guess);
+  // if (typeof guess === "number" && 1 <= guess <= 100) {
   count++;
+  let recent = document.createElement("div");
+  const history = document.getElementById("history");
 
   if (guess == answer) {
     document.getElementById("hint").innerHTML = "정답입니다!";
@@ -11,9 +16,16 @@ function checkAnswer() {
     disableInput();
   } else if (guess > answer) {
     document.getElementById("hint").innerHTML = "입력한 숫자가 너무 큽니다.";
+    recent.innerHTML = `${count}. ${guess}보다 작은 수 입니다.`;
+    history.appendChild(recent);
   } else {
     document.getElementById("hint").innerHTML = "입력한 숫자가 너무 작습니다.";
+    recent.innerHTML = `${count}. ${guess}보다 큰 수 입니다.`;
+    history.appendChild(recent);
   }
+  // } else {
+  //   alert("1부터 100 사이의 숫자를 넣어주세요.");
+  // }
 }
 
 function disableInput() {
